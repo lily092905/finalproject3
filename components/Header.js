0a57458201ea65f516912f3288392318142c1962
lily092905 <209410629@gms.tku.edu.tw>
2021-06-15 04:42:21 +0800

    index

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,10 @@ import styles from '../styles/Header.module.css';
 export default function Header() {
   return (
     <div className={styles.navbar}>
-            <img src="./img/sun.svg" className={styles.logo} />
+            <img src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1623701401/small_sun_57d00d52ab.png" className={styles.logo} />
             <ul>
-                <li className={styles.li}><a className={styles.a} href="#">Product</a></li>
-                <li className={styles.li}><a className={styles.a} href="#">About</a></li>
+                <li className={styles.li}><a className={styles.a} href="#">projrct</a></li>
+                <li className={styles.li}><a className={styles.a} href="#">cards</a></li>
                 <li className={styles.li}><a className={styles.a} href="#">Conduct</a></li>
             </ul>
         </div>
