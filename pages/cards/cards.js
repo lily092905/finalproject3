48840ba91ac8e1da6a9252fd71f52e65bc903607
lily092905 <209410629@gms.tku.edu.tw>
2021-06-20 20:10:31 +0800

    cards

--- pages/cards/cards.js
+++ pages/cards/cards.js
@@ -12,48 +12,115 @@ export default function Home() {
             <title>i am sunshine girl</title>
           </Head>
         <Header />
-        <div className={styles.container}>
-        <div className={styles.card} >
-            <div className={styles.card1}>
-              <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1623665578/small_pexels_mark_stebnicki_2714463_91a15aaba9.jpg" />
+        <div className={styles.outside}>
+          <div className={styles.container}>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624182827/fontcoolnew_443c911a21.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >cool font</h1>
+                </button>
+              </div>
             </div>
-            <div className={styles.card2}>
-              <button className={styles.button}>
-                  <h1 className={styles.h1} >Apple</h1>
-              </button>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181781/large_itsmylife_31cebe427f.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >My web</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181783/ox_cb73853f3d.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >ox</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181780/tku_c317df4b65.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Tku</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181771/small_Dev_5a4ce45fa0.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Dev</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181765/usacards_b1761692d9.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Country card</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181746/large_Micro_e35f528ef9.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Micro</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181740/monster_d94c4a820c.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Monster</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181739/guess_a76f5ddc8e.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Guess</h1>
+                </button>
+              </div>
+            </div>
+          <div className={styles.card} >
+              <div className={styles.card1}>
+                <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1624181737/clickme_7430d63d74.png" />
+              </div>
+              <div className={styles.card2}>
+                <button className={styles.button}>
+                    <h1 className={styles.h1} >Click</h1>
+                </button>
+              </div>
             </div>
           </div>
-        <div className={styles.card} >
-            <div className={styles.card1}>
-              <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1623665571/pexels_laker_6157031_f4a84602bf.jpg" />
-            </div>
-            <div className={styles.card2}>
-              <button className={styles.button}>
-                  <h1 className={styles.h1} >Avocado</h1>
-              </button>
-            </div>
+          <div className={styles.words}>
+            <p className={styles.w}>this</p>
+            <p className={styles.w}>is</p>
+            <p className={styles.w}>my</p>
+            <p className={styles.w}>Portfolio</p>
           </div>
-        <div className={styles.card} >
-            <div className={styles.card1}>
-              <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1623665570/pexels_any_lane_5945725_ae6e846e01.jpg" />
-            </div>
-            <div className={styles.card2}>
-              <button className={styles.button}>
-                  <h1 className={styles.h1} >Papaya</h1>
-              </button>
-            </div>
-          </div>
-        <div className={styles.card} >
-            <div className={styles.card1}>
-              <img className={styles.img} src="https://res.cloudinary.com/dxbvrsg5b/image/upload/v1623665570/pexels_ylanite_koppens_915017_703ba4d86b.jpg" />
-            </div>
-            <div className={styles.card2}>
-              <button className={styles.button}>
-                  <h1 className={styles.h1} >Pineapple</h1>
-              </button>
-            </div>
-          </div>
-
         </div>
         
         
